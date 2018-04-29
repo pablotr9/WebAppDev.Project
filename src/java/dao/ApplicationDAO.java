1279c0ece555023a48cd40ca4755aed9dd1894db
Alejandro <alex_mm_95@hotmail.com>
2018-04-29 21:42:41 +0300

    DAOs

---
+++ src/java/dao/ApplicationDAO.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
+import util.DBConnection;
+import util.Hash;
+
+/**
+ *
+ * @author Andrei
+ */
+public class ApplicationDAO {
+    private Connection connection;
+    private static ApplicationDAO instance;
+
+    public static ApplicationDAO getInstance() {
+        if (instance == null) {
+            instance = new ApplicationDAO();
+        }
+        return instance;
+    }
+
+    private ApplicationDAO() {}
+    
+    public boolean createApplication(String title, String description, int userId, int offerId) {
+        connection=DBConnection.getConnection();
+        try {
+            PreparedStatement stmt = connection.prepareStatement("insert into offers values (default, ?, ?, ?, ?, ?)");
+            stmt.setString(1, title);
+            stmt.setString(2, description);
+            Date utilDate = new Date();
+            java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
+            stmt.setDate(3, sqlDate);
+            stmt.setInt(4, offerId);
+            stmt.setInt(5, userId);
+            stmt.executeUpdate();
+            connection.commit();
+            return true;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return false;
+    }
+}
